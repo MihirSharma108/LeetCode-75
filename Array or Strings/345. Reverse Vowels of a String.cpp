910d46aadc44e47901d3e54f547539815b9155b0
Mihir Sharma <mihirsharma943@gmail.com>
2024-02-03 22:31:30 +0530

    Add files via upload

---
+++ Array or Strings/345. Reverse Vowels of a String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checker(char c) {
+        c=tolower(c);
+        return (c=='a'|| c=='e'|| c=='i'|| c=='o'|| c=='u');
+    } 
+
+    string reverseVowels(string s) {
+        int n=s.size();
+        int i=0,j=n-1;
+        while(i<j) {
+            if(checker(s[i]) && checker(s[j])) {
+                swap(s[i], s[j]);
+                i++; j--;
+            }
+            else if(checker(s[i])==false) i++;
+            else if(checker(s[j])==false) j--;
+        }
+
+        return s;
+    }
+};
