b8ed0810a6b0ae5488e283bf93f0e032141c9bf8
Mihir Sharma <mihirsharma943@gmail.com>
2024-02-07 21:53:48 +0530

    Update 605. Can Place Flowers.cpp

--- Array or Strings/605. Can Place Flowers.cpp
+++ Array or Strings/605. Can Place Flowers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
+        int count = 0;    //we are taking the count variable and initializing it with 0
         for(int i=0; i<flowerbed.size(); i++) {
             if(flowerbed[i]==0) {
                 bool emptyLeftPlot = (i==0) || (flowerbed[i-1]==0);
