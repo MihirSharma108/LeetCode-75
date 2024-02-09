9b521319d3ec24e5e9a1ae5c8b36280bc0e6c26d
Mihir Sharma <mihirsharma943@gmail.com>
2024-02-09 22:07:54 +0530

    Update 1071. Greatest Common Divisor of Strings.cpp

--- Array or Strings/1071. Greatest Common Divisor of Strings.cpp
+++ Array or Strings/1071. Greatest Common Divisor of Strings.cpp
@@ -3,7 +3,7 @@ public:
     string gcdOfStrings(string str1, string str2) {
         // here we are checking the str1+str2 is equal to str+str1 
         if(str1+str2 != str2+str1) return "";
-        // here we are using the substr function 
+        // here we are using the substr function  
         return str1.substr(0, gcd(str1.size(), str2.size()));    //returning the final ans 
     }
 };
