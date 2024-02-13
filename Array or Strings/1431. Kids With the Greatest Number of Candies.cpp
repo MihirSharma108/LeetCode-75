489103922d9b93f625a1b074018a8186c3f6d596
Mihir Sharma <mihirsharma943@gmail.com>
2024-02-14 00:04:04 +0530

    Update 1431. Kids With the Greatest Number of Candies.cpp

--- Array or Strings/1431. Kids With the Greatest Number of Candies.cpp
+++ Array or Strings/1431. Kids With the Greatest Number of Candies.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        int maxiCan = INT_MIN;
+        int maxiCan = INT_MIN;    //here we are storing the maxican with the minimum integer value possible because we have to find the maximum one 
         for(auto i: candies) { 
             maxiCan = max(maxiCan, i);
         }
